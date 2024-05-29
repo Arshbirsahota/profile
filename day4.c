e1c283763d2879ec1e001666fa31512124f511f5
Arshbirsahota <arshbirsingh05@gmail.com>
2024-05-29 11:50:29 +0530

    Create day4.c

---
+++ day4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int principalamount;
+    float rateofinterest;
+
+    printf("Enter Your principal amount:");
+    scanf("%d",&principalamount);
+    printf("Enter Your rate of interest:");
+    scanf("%f",&rateofinterest);
+    printf("Enter your time period");
+    scanf("%t",&timeperiod);
+
+
+
+    printf("\n principal amount=%p",principal amount);
+    printf("\n rate of interest=%f",rate of interest);
+    printf("\n time period=")
