5f76b0cc43c947db52a1208d4c0ee4902c617004
Arshbirsahota <arshbirsingh05@gmail.com>
2024-05-29 11:30:29 +0530

    change

---
+++ day1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    printf("\t\t Report card");
+printf("\n    Name\t:\tABC");
+printf("\n   Roll no\t:\t 1");
+printf("\n Subject\tInternal(25)\t External(75)\t total");
+printf("\n        c\t    20\t          70\t           90\t");
+printf("\n        java\t    20\t          70\t           90\t");
+printf("\n        PHP\t    20\t          70\t           90\t");
+printf("\n        SQL\t    20\t          70\t           90\t");
+printf("\n        HTML\t    20\t          70\t           90\t");
+                                    printf("\n \t\t\t\t\t total marks 450");
+                                     printf("\n \t\t\t\t\t percentage 90%%\n");
+
+
+}
+
